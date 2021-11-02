180e108b8775ced15e1d00261708e186808452e5
Bulat Sharipov <bulat20031006@gmail.com>
2021-11-02 16:05:24 +0300

    Изменения в интерфейсе UserInfoDao и UserAccountDao

--- domain/interfaces/dao/user_account_dao.py
+++ domain/interfaces/dao/user_account_dao.py
@@ -1,8 +1,15 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from domain.entities import UserAccount
 from domain.interfaces.dao.base import IDao
 
 
 class IUserAccountDao(IDao[UserAccount], ABC):
-    pass
+
+    @abstractmethod
+    def from_login(self, login: str) -> UserAccount | None:
+        pass
+
+    @abstractmethod
+    def from_user_id(self, user_id: int) -> UserAccount | None:
+        pass
--- domain/interfaces/dao/user_info_dao.py
+++ domain/interfaces/dao/user_info_dao.py
@@ -1,8 +1,11 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from domain.entities import UserInfo
 from domain.interfaces.dao.base import IDao
 
 
 class IUserInfoDao(IDao[UserInfo], ABC):
-    pass
+
+    @abstractmethod
+    def from_user_id(self, user_id: int) -> UserInfo | None:
+        pass
